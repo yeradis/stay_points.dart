abd94862b36459b6871cb09da7fa01b810262eb5
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-10-15 09:22:45 +0200

    Update coverage helper script, now detects if the coverage package is missing and activate it if necessary

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+set -e
 
-pub get
-pub global activate coverage
+setupIfNeeded(){
+    if [ ! -f ".packages" ]; then
+        echo "Missing .packages... setting up now!"
+        echo -e "\n\n"
+        pub get
+        echo -e "\n\n"
+    fi
+
+    PACKAGE_LIST="$(pub global list)"
+
+    if [[ ! $PACKAGE_LIST == *"coverage"* ]]; then
+        echo "Missing coverage package... activating now!"
+        echo -e "\n\n"
+        pub get
+        pub global activate coverage
+        echo -e "\n\n"
+    fi
+}
+
+setupIfNeeded
+
 dart --pause-isolates-on-exit --enable-vm-service=8111 test/test_all.dart & sleep 5
 pub global run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates
 pub global run coverage:format_coverage --packages=.packages -i coverage.json -o coverage.lcov --lcov --report-on=lib/
